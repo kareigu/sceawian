24c11ccd1ee4ca2ef30b7a9b9dadc41ce3e8c998
karei <mail@karei.dev>
2024-06-02 15:51:32 +0300

    Run mirror updating on an interval
    Currently this is set to rerun every 20 seconds for testing purposes.
    In future this should be configurable through a config file.

--- src/main.rs
+++ src/main.rs
@@ -21,35 +21,43 @@ async fn main() -> Result<()> {
 
     let mut handles = tokio::task::JoinSet::new();
 
-    for file in std::path::Path::new("repos").read_dir()? {
-        let file = match file {
-            Ok(f) => f,
-            Err(e) => {
-                error!("failed getting file: {}", e);
-                continue;
-            }
+    let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(20));
+    let mut prev_time = tokio::time::Instant::now();
+
+    loop {
+        let time = interval.tick().await;
+        let duration = time.duration_since(prev_time).as_secs();
+        info!("updating mirrors after waiting for {} seconds", duration);
+        prev_time = time;
+
+        let Ok(repos_dir) = std::path::Path::new("repos").read_dir() else {
+            continue;
         };
 
-        if let Some(ext) = file.path().extension() {
-            if ext != "toml" {
-                continue;
+        for file in repos_dir {
+            let file = match file {
+                Ok(f) => f,
+                Err(e) => {
+                    error!("failed getting file: {}", e);
+                    continue;
+                }
+            };
+
+            if let Some(ext) = file.path().extension() {
+                if ext != "toml" {
+                    continue;
+                }
+            }
+
+            handles.spawn(run_actions(file.path()));
+        }
+
+        while let Some(res) = handles.join_next().await {
+            match res {
+                Err(e) => error!("joining task failed: {}", e),
+                Ok(Err(e)) => error!("{}", e),
+                Ok(Ok(details)) => info!("{}: mirroring finished", details.name),
             }
         }
-
-        handles.spawn(run_actions(file.path()));
     }
-
-    while let Some(res) = handles.join_next().await {
-        if let Err(e) = res {
-            error!("joining task failed: {}", e);
-            continue;
-        }
-
-        match res.unwrap() {
-            Ok(details) => info!("{}: mirroring finished", details.name),
-            Err(e) => error!("{}", e),
-        }
-    }
-
-    return Ok(());
 }
